0834aae33ae6e84548e6a28301f6ed2aa7363daf
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-02 20:53:53 +1000

    Updated Links

--- PROJECT/updatePro.php
+++ PROJECT/updatePro.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+if ( $_SESSION['STAFF_ID'] != 1) {
+	$_SESSION['message'] = "You mus log in before viewing this page";
+	header("location: ../ACCESS/error.php");
+	}
+	else {
+	$id = $_SESSION['STAFF_ID'];
+	$staff_firstname = $_SESSION['STAFF_FIRSTNAME'];
+	$staff_lastname = $_SESSION['STAFF_LASTNAME'];
+	}
+?>
 <!doctype html>
 
 <html>
@@ -7,250 +19,8 @@
 <meta charset="utf-8">
 
 <title>Update Project</title>
+<link rel="stylesheet" type="text/css" href="../STYLES/stylesheet.css">
 
-<style>
-
-body {
-
-  padding-bottom: 40px;
-
-  background-color: #eee;
-
-}
-
-.form-signin {
-
-  max-width: 330px;
-
-  padding: 15px;
-
-  margin: 0 auto;
-
-}
-
-.form-signin .form-signin-heading,
-
-.form-signin .checkbox {
-
-  margin-bottom: 10px;
-
-}
-
-.form-signin .checkbox {
-
-  font-weight: normal;
-
-}
-
-.form-signin .form-control {
-
-  position: relative;
-
-  height: auto;
-
-  -webkit-box-sizing: border-box;
-
-     -moz-box-sizing: border-box;
-
-          box-sizing: border-box;
-
-  padding: 10px;
-
-  font-size: 16px;
-
-}
-
-.form-signin .form-control:focus {
-
-  z-index: 2;
-
-}
-
-.form-signin input[type="email"] {
-
-  margin-bottom: -1px;
-
-  border-bottom-right-radius: 0;
-
-  border-bottom-left-radius: 0;
-
-}
-
-.form-signin input[type="password"] {
-
-  margin-bottom: 10px;
-
-  border-top-left-radius: 0;
-
-  border-top-right-radius: 0;
-
-}
-
-.Header {
-
-	background-color: #333;
-
-	font-family: Arial, Helvetica, sans-serif;
-
-	color: white;
-
-	text-align: center;
-
-	padding: 5px;
-
-}
-
-.Footer {
-
-	padding: 20px;
-
-	background-color: #333;
-
-	color: white;
-
-	text-align: center;
-
-	font-family: Arial, Helvetica, sans-serif;
-
-}
-
-p {
-
-	margin-left: 40px;
-
-	margin-right: 50px;
-
-	font-family: Arial, Helvetica, sans-serif;
-
-}
-
-h2 {
-
-	margin-left: 40px;
-
-	font-family: Arial, Helvetica, sans-serif;
-
-}
-
-/* The navagation bar for the site, linked to the drop downs */
-
-	.navbar {
-
-    overflow: hidden;
-
-    background-color: #333;
-
-    font-family: Arial, Helvetica, sans-serif;
-
-}
-
-.navbar a {
-
-    float: left;
-
-    font-size: 16px;
-
-    color: white;
-
-    text-align: center;
-
-    padding: 14px 100px;
-
-    text-decoration: none;
-
-}
-
-/* This is for the drop down in the navigation bar */
-
-.dropdown {
-
-    float: left;
-
-    overflow: hidden;
-
-}
-
-.dropdown .dropbtn {
-
-    font-size: 16px;
-
-    border: none;
-
-    outline: none;
-
-    color: white;
-
-    padding: 14px 16px;
-
-    background-color: inherit;
-
-    font-family: inherit;
-
-    margin: 0;
-
-}
-
-.navbar a:hover, .dropdown:hover .dropbtn {
-
-    background-color: red;
-
-}
-
-.dropdown-content {
-
-    display: none;
-
-    position: absolute;
-
-    background-color: #f9f9f9;
-
-    min-width: 160px;
-
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-
-    z-index: 1;
-
-}
-
-.dropdown-content a {
-
-    float: none;
-
-    color: black;
-
-    padding: 12px 16px;
-
-    text-decoration: none;
-
-    display: block;
-
-    text-align: left;
-
-}
-
-.dropdown-content a:hover {
-
-    background-color: #ddd;
-
-}
-
-.dropdown:hover .dropdown-content {
-
-    display: block;
-
-}
-
-/*Picture wont float right?*/
-
-#stockpic
-
-{
-
-    float:right;
-
-}
-
-</style>
 </head>
 
 
@@ -266,40 +36,28 @@ h2 {
 
 
 <div class="navbar">
-
-	<a href="#Home">Home</a>
-
-	<a href="#Projects">Projects</a>
-
-	<a href="#Conatacts">Contacts</a>
-
-	<a href="#About Us">About Us</a>
-
+	<a href="../PAGES/STAFFHOME.php">Home</a>
 	<div class="dropdown">
-
-		<button class="dropbtn">Login
-
+		<button class="dropbtn">Projects
 			<i class="fa fa-caret-down"></i>
-
 		</button>
-
-		<div class="dropdown-content">
-
-			<a href="#Students">Students</a>
-
-			<a href="#Teachers">Teachers</a>
-
-		</div>
-
-	</div>
-
+        	<div class="dropdown-content">
+            		<a href="../PROJECT/ADDPROJECT.php">Create a Project</a>
+            		<a href="../PROJECT/NEWGROUP.php">Make Groups</a>
+            		<a href="../PROJECT/GROUPLIST.php">Group List</a>
+            		<a href="../PROJECT/PROJECTLIST.php">Previous Projects</a>
+			<a href="../PROJECT/updatePro.php">Update Projects</a>
+        	</div>
+	<a href="../PAGES/STAFFCONTACT.php">Contacts</a>
+	<a href="../PAGES/STAFFABOUTUS.php">About Us</a>
+	<a href="../ACCESS/stafflogout.php">Logout</a>
 </div>
 
 
 <div id="contents" >
 <h2>Update Project</h2>
 <p>Project Number </p> 
-<p><form action="updateProcess.php" method="post"></p>
+<p><form action="../PROJECT/updateProcess.php" method="post"></p>
 <p><input type="text" name="number"/></p>
 <p>Project Brief </p> 
 <p><textarea name="brief" rows="5" cols="40"></textarea></br></p>
