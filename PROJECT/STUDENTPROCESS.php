207968a517d0a40167b64bb16288d87889781506
tia0518 <37648577+tia0518@users.noreply.github.com>
2018-05-07 20:45:01 +1000

    Create STUDENTPROCESS.php

---
+++ PROJECT/STUDENTPROCESS.php
@@ -0,0 +1,131 @@
+<?php
+session_start();
+if ( $_SESSION['STAFF_ID'] != 1) {
+	$_SESSION['message'] = "You must log in before viewing this page";
+	header("location: ../ACCESS/error.php");
+	}
+	else {
+	$id = $_SESSION['STAFF_ID'];
+	$staff_firstname = $_SESSION['STAFF_FIRSTNAME'];
+	$staff_lastname = $_SESSION['STAFF_LASTNAME'];
+	}
+?>
+<!doctype html>
+
+<html>
+
+<head>
+
+<meta charset="utf-8">
+
+<title>Template</title>
+<link rel="stylesheet" type="text/css" href="../STYLES/stylesheet.css">
+<style>
+/*Picture wont float right?*/
+
+#stockpic
+
+{
+
+    float:right;
+
+}
+
+</style>
+
+</head>
+
+
+
+<body>
+
+<div class="Header">
+
+	<h1>The Corral Project</h1>
+
+</div>
+
+
+
+<div class="navbar">
+	<a href="../PAGES/STAFFHOME.php">Home</a>
+	<div class="dropdown">
+		<button class="dropbtn">Projects
+			<i class="fa fa-caret-down"></i>
+		</button>
+        	<div class="dropdown-content">
+            		<a href="../PROJECT/ADDPROJECT.php">Create Project</a>
+								<a href="../PROJECT/PROJECTLIST.php">Project List</a>
+								<a href="../PROJECT/PROJECTUPDATE.PHP">Update Project</a>
+            		<a href="../PROJECT/ADDGROUP.php">Create Group</a>
+            		<a href="../PROJECT/GROUPLIST.php">Group List</a>
+								<a href="../PROJECT/GROUPUPDATE.PHP">Update Group</a>
+                    <a href="../PROJECT/STUDENTLIST.php">Student List</a>
+					<a href="../PROJECT/STAFFLIST.php">Staff List</a>
+							</div>
+	</div>
+	<a href="../PAGES/STAFFCONTACT.php">Contacts</a>
+	<a href="../PAGES/STAFFABOUTUS.php">About Us</a>
+	<a href="../ACCESS/stafflogout.php">Logout</a>
+</div>
+
+
+
+<div id="contents">
+
+  <h2>Updated student information</h2>
+	<?php
+ require("../DATABASE/CONNECTDB.php");
+
+		
+		$number=$_SESSION['number'];
+		$firstname=$_POST['STUDENT_FIRSTNAME'];
+		$lastname=$_POST['STUDENT_LASTNAME'];
+		$location=$_POST['STUDENT_LOCATION'];
+		$email=$_POST['STUDENT_EMAIL'];
+
+
+	     if(empty($location)){
+	        echo "<p>you need to input location</p>";
+	    }else if(empty($email)){
+			 echo "<p>you need to input email</p>";
+		}else if(empty($firstname)){
+			 echo "<p>you need to input firstname</p>";
+		}else if(empty($lastname)){
+			 echo "<p>you need to input lastname</p>";
+		}else{
+
+
+
+	        $sql="UPDATE STUDENT SET STUDENT_LOCATION='$location',STUDENT_EMAIL='$email',STUDENT_FIRSTNAME='$firstname',STUDENT_LASTNAME='$lastname' WHERE STUDENT_NUM=$number";
+
+	        $b=mysqli_query($CON,$sql);
+
+	         if(!$b){
+	                echo "<p>Failed to update information</p>";
+	          }else{
+	                if(mysqli_affected_rows($CON)>0){
+	                    echo "<p>information successfully updated</p>";
+	                    echo "<p><a href='STUDENTLIST.PHP'>Back to student list</a></p>";
+	                }else{
+	                    return "<p>not affected rows</p>";
+	                }
+	            }
+
+	        mysqli_close($CON);
+	    }
+
+	?>
+
+
+<hr>
+
+<div class="Footer">
+
+	<h4>This is copyrighted by Deakin and the project group 29</h4>
+
+</div>
+
+</body>
+
+</html>
