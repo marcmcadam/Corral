53219a7b59c90c777a65bab8382fbf5a8e33d094
tia0518 <37648577+tia0518@users.noreply.github.com>
2018-05-07 20:45:35 +1000

    Create STUDENTUPDATE.php

---
+++ PROJECT/STUDENTUPDATE.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+/*if ( $_SESSION['STAFF_ID'] != 1) {
+	$_SESSION['message'] = "You mus log in before viewing this page";
+	header("location: ../ACCESS/error.php");
+	}
+	else {
+	$id = $_SESSION['STAFF_ID'];
+	$staff_firstname = $_SESSION['STAFF_FIRSTNAME'];
+	$staff_lastname = $_SESSION['STAFF_LASTNAME'];
+	}*/
+	
+$_SESSION['number']=$_GET['number'];
+
+?>
+<!doctype html>
+
+<html>
+
+<head>
+
+<meta charset="utf-8">
+
+<title>Template</title>
+<link rel="stylesheet" type="text/css" href="../STYLES/stylesheet.css">
+<style>
+/*Picture wont float right?*/
+#stockpic
+{
+    float:right;
+}
+</style>
+
+</head>
+
+
+
+<body>
+
+<div class="Header">
+
+	<h1>The Corral Project</h1>
+
+</div>
+
+
+
+<div class="navbar">
+	<a href="../PAGES/STAFFHOME.php">Home</a>
+	<div class="dropdown">
+		<button class="dropbtn">Projects
+			<i class="fa fa-caret-down"></i>
+		</button>
+        	<div class="dropdown-content">
+            		<a href="../PROJECT/ADDPROJECT.php">Create Project</a>
+								<a href="../PROJECT/PROJECTLIST.php">Project List</a>
+								<a href="../PROJECT/PROJECTUPDATE.PHP">Update Project</a>
+            		<a href="../PROJECT/ADDGROUP.php">Create Group</a>
+            		<a href="../PROJECT/GROUPLIST.php">Group List</a>
+								<a href="../PROJECT/GROUPUPDATE.PHP">Update Group</a>
+                <a href="../PROJECT/STUDENTLIST.php">Student List</a>
+                <a href="../PROJECT/STAFFLIST.php">Staff List</a>
+				
+							</div>
+	</div>
+	<a href="../PAGES/STAFFCONTACT.php">Contacts</a>
+	<a href="../PAGES/STAFFABOUTUS.php">About Us</a>
+	<a href="../ACCESS/stafflogout.php">Logout</a>
+</div>
+
+
+
+<div id="contents">
+<h2>Update Student List</h2>
+<p><form action="../PROJECT/STUDENTPROCESS.PHP" method="post"></p>
+<p>Student number <?php echo $_GET['number'];?></p>
+
+<p>Firstname </p>
+<p><input type="text" name="STUDENT_FIRSTNAME" value="<?php echo $_GET['firstname'];?>"/></p>
+<p>Lastname </p>
+<p><input type="text" name="STUDENT_LASTNAME" value="<?php echo $_GET['lastname'];?>"/></p>
+<p>Location </p>
+<p><input type="text" name="STUDENT_LOCATION" value="<?php echo $_GET['location'];?>"/></p>
+<p>Email </p>
+<p><input type="text" name="STUDENT_EMAIL" value="<?php echo $_GET['email'];?>"/></p>
+
+<p><input type="submit" value="update">&nbsp&nbsp<input type="reset" value="reset"></p>
+</form>
+</div>
+
+
+
+<hr>
+
+<div class="Footer">
+
+	<h4>This is copyrighted by Deakin and the project group 29</h4>
+
+</div>
+
+</body>
+
+</html>
