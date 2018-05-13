b1e263457f79ad4bc5ff68d347e4f8bcd48ba8b9
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-13 19:23:41 +1000

    Updated for new form look

--- ACCESS/stafflogin.php
+++ ACCESS/stafflogin.php
@@ -32,8 +32,8 @@ require('../DATABASE/CONNECTDB.PHP');
       <form method="POST">
         <h2>Please Log In</h2>
     <span class="input-group-addon" id="basic-addon1"></span>
-    <input type="text" name="STAFF_ID" placeholder="STAFF ID" required><br><br>
-        <input type="password" name="STAFF_PASSWORD" id="inputPassword" placeholder="Password" required><br><br>
+    <input type="text" name="STAFF_ID" placeholder="STAFF ID" id="ip2" required><br><br>
+        <input type="password" name="STAFF_PASSWORD" id="ip2" placeholder="Password" required><br><br>
         <button type="submit">LOGIN</button>
         <button type="button" onclick="location.href='../PAGES/STAFFREGISTRATION.HTML';" vaule="Register" />REGISTER</button><br><br>
       </form>
