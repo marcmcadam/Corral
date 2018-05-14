d5ac84cc7b692f2ba21f9fa8978bb4f68f426f4a
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-14 20:20:09 +1000

    Updated to say Corral, not Corral Projects

--- PAGES/STAFFABOUTUS.php
+++ PAGES/STAFFABOUTUS.php
@@ -19,7 +19,9 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 </head>
 
 <body>
-
+<div class="Header">
+	<h1>Corral</h1>
+</div>
 
 	<div class="navbar">
 		<a href="../PAGES/STAFFHOME.php">Home</a>
@@ -45,22 +47,22 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 
 <div id="contents">
   <h2>About Us</h2>
-<p>Welcome to the Corral Project 'about us' page. Here you will find details of how this project began along with some frequently asked questions as listed below:</p>
+<p>Welcome to the Corral 'about us' page. Here you will find details of how this project began along with some frequently asked questions as listed below:</p>
 
 </div>
 
 <hr>
-<h2>Origins of the Corral Project</h2>
-<p>The Corral Project was created for the SIT374 'Project Design' unit at Deakin University, whereby we were required to create a prototype that serves as a solution to the pre-existing problem of sorting individuals into equal groups based on skill.</p>
+<h2>Origins of Corral</h2>
+<p>The Corral was created for the SIT374 'Project Design' unit at Deakin University, whereby we were required to create a prototype that serves as a solution to the pre-existing problem of sorting individuals into equal groups based on skill.</p>
 
 <hr>
 <h2>Frequently Asked Questions</h2>
 <h3>How does it work?</h3>
 <p>The process begins with users completing our quick survey, detailing their skills on both a technical and personal level. From there, the survey is submitted and added to our databases. From there your job as the user is done! When new projects are added, users can then be allocated certain roles, which is dependant on not only the users proficiency in certain skills, but also the skills required for the task at hand. Once the team has been finalised, users may access details of the project and contact other members of the group.</p>
-<h3>Who can use the Corral Project?</h3>
-<p>The Corral Project has been primarily designed for students and teachers as an interface for sharing upcoming projects that students will take part in. However, The Corral Project can also be utilised by employers who desire a platform for showcase upcoming projects that require group work, as the Corral Project takes away the pain and time of assigning equal teams to different tasks. </p>
+<h3>Who can use Corral?</h3>
+<p>Corral has been primarily designed for students and teachers as an interface for sharing upcoming projects that students will take part in. However, Corral can also be utilised by employers who desire a platform for showcase upcoming projects that require group work, as Corral takes away the pain and time of assigning equal teams to different tasks. </p>
 <h3>What options do I have if I don't get the project I want?</h3>
-<p>Changing projects is an option at the discretion of your teacher or employer, as we at the Corral Project do not oversee each team when they have been successful placed to a project.</p>
+<p>Changing projects is an option at the discretion of your teacher or employer, as we at the Corral do not oversee each team when they have been successful placed to a project.</p>
 <h3>Can I decline a project after already being accepted into it?</h3>
 <p>This depends on the nature of the project (compulsory or on a volunteer-basis) but once again is at the discretion of those who have created the project. Please contact your supervisor directly.</p>
 <p>&nbsp;</p>
