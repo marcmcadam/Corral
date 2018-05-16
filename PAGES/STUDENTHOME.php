d94f703cce889e350a1ac56623a057bbfc59350d
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-16 11:16:30 +1000

    Update links

--- PAGES/STUDENTHOME.php
+++ PAGES/STUDENTHOME.php
@@ -3,7 +3,7 @@ session_start();
 
 if ( $_SESSION['STUDENT_ID'] != 1) {
 	$_SESSION['message'] = "You mus log in before viewing this page";
-	header("location: ../ACCESS/error.php");
+	header("location: ../ACCESS/error");
 	}
 	else {
 	$id = $_SESSION['STUDENT_ID'];
@@ -28,14 +28,14 @@ if ( $_SESSION['STUDENT_ID'] != 1) {
 
 <body>
 <div class="Header">
-
+	<h1>Corral</h1>
 </div>
 <div class="navbar">
-	<a href="../PAGES/STUDENTHOME.php">Home</a>
-	<a href="../SURVEY/STUDENTSURVEY.PHP">Survey</a>
-	<a href="../PAGES/STUDENTCONTACT.php">Contacts</a>
-	<a href="../PAGES/STUDENTABOUTUS.php">About Us</a>
-	<a href="../Access/LOGOUT.php">Logout</a>
+	<a href="../PAGES/STUDENTHOME">Home</a>
+	<a href="../SURVEY/STUDENTSURVEY">Survey</a>
+	<a href="../PAGES/STUDENTCONTACT">Contacts</a>
+	<a href="../PAGES/STUDENTABOUTUS">About Us</a>
+	<a href="../Access/LOGOUT">Logout</a>
 </div>
 <div id="stockpic">
   <img src="stockppl.jpg" alt"">
@@ -44,7 +44,7 @@ if ( $_SESSION['STUDENT_ID'] != 1) {
 <p>This site aims to provide staff and students with a platform to view and request access to upcoming projects. Through this platform, staff and teachers may upload projects for students, including details of the skills sets and numbers required for the job. Students who register can then complete a quick survey to determine their levels or proficency in certain areas such as IT and communication. Corral takes this information, and provides Teachers with a list of suitable candidates for the positions available.
 
 
-  <br><br>For more information, please click <a href="STUDENTABOUTUS.PHP">here</a></p>
+  <br><br>For more information, please click <a href="../PAGES/STUDENTABOUTUS">here</a></p>
   </div>
 </div>
 
