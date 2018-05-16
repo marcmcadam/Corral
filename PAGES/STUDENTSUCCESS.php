77698a0181540fe17fa9a346596298a9a982b0ac
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-16 11:18:13 +1000

    Update links

--- PAGES/STUDENTSUCCESS.php
+++ PAGES/STUDENTSUCCESS.php
@@ -2,7 +2,7 @@
 session_start();
 if ( $_SESSION['STUDENT_ID'] != 1) {
 	$_SESSION['message'] = "You mus log in before viewing this page";
-	header("location: ../ACCESS/error.php");
+	header("location: ../ACCESS/error");
 	}
 	else {
 	$id = $_SESSION['STUDENT_ID'];
@@ -39,25 +39,23 @@ $headers = "From: $email_from \r\n";
 mail($to,$email_subject,$email_body,$headers);
  ?>
  <div class="Header">
+	<h1>Corral</h1>
+</div>
 
 
 
- </div>
-
-
-
-<<div class="navbar">
-	<a href="../PAGES/STUDENTHOME.php">Home</a>
-	<a href="../SURVEY/STUDENTSURVEY.PHP">Survey</a>
-	<a href="../PAGES/STUDENTCONTACT.php">Contacts</a>
-	<a href="../PAGES/STUDENTABOUTUS.php">About Us</a>
-	<a href="../Access/LOGOUT.php">Logout</a>
+<div class="navbar">
+	<a href="../PAGES/STUDENTHOME">Home</a>
+	<a href="../SURVEY/STUDENTSURVEY">Survey</a>
+	<a href="../PAGES/STUDENTCONTACT">Contacts</a>
+	<a href="../PAGES/STUDENTABOUTUS">About Us</a>
+	<a href="../Access/LOGOUT">Logout</a>
 </div>
 
 <div id="contents">
 <h2>Question Succesfully Submitted!</h2>
 <p>Thank you for your feedback! We at Corral love to hear from those who use and value our website. We will reply to your comments as swiftly as possible.
-<a href="../PAGES/STUDENTHOME.html"><h2 align="center">Back to Home Page</h2></a>
+<a href="../PAGES/STUDENTHOME"><h2 align="center">Back to Home Page</h2></a>
 <div class="Footer">
 	<h4>This is copyrighted by Deakin and the project group 29</h4>
 </div>
