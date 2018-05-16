6ee8f62a5b98aa478fe240f0d3d1197f1d326e71
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-16 21:14:57 +1000

    Updated dropdown

--- PROJECT/PROJECTSEARCH.php
+++ PROJECT/PROJECTSEARCH.php
@@ -36,7 +36,7 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 						<li><a href ="../PROJECT/ADDPROJECT">Create Project</a></li>
 						<li><a href ="../PROJECT/updatePro">Update Project</a></li>
 						<li><a href ="../PROJECT/PROJECTLIST.php">List Projects</a></li>
-						<li><a href ="PROJECTSEARCH">Project Search</a></li>
+						<li><a href ="../PROJECT/PROJECTSEARCH">Project Search</a></li>
 					</ul>
 				</li>
 				<li><a href ="#">Groups</a>
