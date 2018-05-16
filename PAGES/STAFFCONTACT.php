e4d8bab381a6bd0a04fb953bee734cd85cf79c62
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-16 11:26:48 +1000

    Updated links

--- PAGES/STAFFCONTACT.php
+++ PAGES/STAFFCONTACT.php
@@ -3,7 +3,7 @@ session_start();
 
 if ( $_SESSION['STAFF_ID'] != 1) {
 	$_SESSION['message'] = "You mus log in before viewing this page";
-	header("location: ../ACCESS/error.php");
+	header("location: ../ACCESS/error");
 	}
 	else {
 	$id = $_SESSION['STAFF_ID'];
@@ -21,29 +21,29 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 
 <body>
 <div class="Header">
-
+	<h1>Corral</h1>
 </div>
 
 <div class="navbar">
-	<a href="../PAGES/STAFFHOME.php">Home</a>
-	<div class="dropdown">
-		<button class="dropbtn">Projects
-			<i class="fa fa-caret-down"></i>
-		</button>
-        	<div class="dropdown-content">
-            		<a href="../PROJECT/ADDPROJECT.php">Create Project</a>
-								<a href="../PROJECT/PROJECTLIST.php">Project List</a>
-								<a href="../PROJECT/PROJECTUPDATE.PHP">Update Project</a>
-            		<a href="../PROJECT/ADDGROUP.php">Create Group</a>
-            		<a href="../PROJECT/GROUPLIST.php">Group List</a>
-								<a href="../PROJECT/GROUPUPDATE.PHP">Update Group</a>
-                <a href="../PROJECT/STUDENTLIST.php">Student List</a>
-                <a href="../PROJECT/STAFFLIST.php">Staff List</a>
-							</div>
-	</div>
-	<a href="../PAGES/STAFFCONTACT.php">Contacts</a>
-	<a href="../PAGES/STAFFABOUTUS.php">About Us</a>
-	<a href="../ACCESS/stafflogout.php">Logout</a>
+		<a href="../PAGES/STAFFHOME">Home</a>
+		<div class="dropdown">
+			<button class="dropbtn">Projects
+				<i class="fa fa-caret-down"></i>
+			</button>
+	        	<div class="dropdown-content">
+	            		<a href="../PROJECT/ADDPROJECT">Create Project</a>
+				<a href="../PROJECT/PROJECTLIST">Project List</a>
+				<a href="../PROJECT/PROJECTUPDATE">Update Project</a>
+	            		<a href="../PROJECT/ADDGROUP">Create Group</a>
+	            		<a href="../PROJECT/GROUPLIST">Group List</a>
+				<a href="../PROJECT/GROUPUPDATE">Update Group</a>
+	                	<a href="../PROJECT/STUDENTLIST">Student List</a>
+	                	<a href="../PROJECT/STAFFLIST">Staff List</a>
+			</div>
+		</div>
+		<a href="../PAGES/STAFFCONTACT">Contacts</a>
+		<a href="../PAGES/STAFFABOUTUS">About Us</a>
+		<a href="../ACCESS/stafflogout">Logout</a>
 </div>
 
 <div id="contents">
@@ -53,7 +53,7 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 <hr>
 <h2>Tell Us What You Think!</h2>
 <p>Do you have a question that we didn't talk about in our FAQ section or would you like to give us some feedback? Insert your details below and we will get back to you as soon as possible!
-<form style="margin-left: 40px" name="emailcontact" onSubmit="return validate(emailcontact)" method="post" action="../PAGES/STAFFSUCCESS.php">
+<form style="margin-left: 40px" name="emailcontact" onSubmit="return validate(emailcontact)" method="post" action="../PAGES/STAFFSUCCESS">
 	First Name: <input type="text" name="firstname" id="ip2" required><br><br>
 	Last Name: <input type="text" name="lastname" id="ip2" required><br><br>
 	Student ID: <input type="text" name="id" maxlength="9" id="ip2" required><br><br>
