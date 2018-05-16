8fdde41c6ccf3df8366d6e20ca40e50c3fe75193
BaileyVanDoorn <37647647+BaileyVanDoorn@users.noreply.github.com>
2018-05-16 19:56:50 +1000

    Update STUDENTLIST.php

--- PROJECT/STUDENTLIST.php
+++ PROJECT/STUDENTLIST.php
@@ -320,7 +320,7 @@ mysqli_close($CON);
 
 <div class="Footer">
 
-	<h4>This is copyrighted by Deakin and the project group 29</h4>
+	<h4>© Copyright Deakin University & Group 29 2018</h4>
 
 </div>
 
