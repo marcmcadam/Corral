9afc323282413c19d8ebd40d4a473a15496937d3
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-16 11:27:56 +1000

    Updated links

--- PAGES/STAFFHOME.php
+++ PAGES/STAFFHOME.php
@@ -3,7 +3,7 @@ session_start();
 
 if ( $_SESSION['STAFF_ID'] != 1) {
 	$_SESSION['message'] = "You mus log in before viewing this page";
-	header("location: ../ACCESS/error.php");
+	header("location: ../ACCESS/error");
 	}
 	else {
 	$id = $_SESSION['STAFF_ID'];
@@ -28,29 +28,29 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 
 <body>
 <div class="Header">
-
+	<h1>Corral</h1>
 </div>
 
 <div class="navbar">
-	<a href="../PAGES/STAFFHOME.php">Home</a>
-	<div class="dropdown">
-		<button class="dropbtn">Projects
-			<i class="fa fa-caret-down"></i>
-		</button>
-        	<div class="dropdown-content">
-            		<a href="../PROJECT/ADDPROJECT.php">Create Project</a>
-								<a href="../PROJECT/PROJECTLIST.php">Project List</a>
-								<a href="../PROJECT/PROJECTUPDATE.PHP">Update Project</a>
-            		<a href="../PROJECT/NEWGROUP.php">Create Group</a>
-            		<a href="../PROJECT/GROUPLIST.php">Group List</a>
-								<a href="../PROJECT/PROJECTUPDATE.PHP">Update Group</a>
-                <a href="../PROJECT/STUDENTLIST.php">Student List</a>
-                <a href="../PROJECT/STAFFLIST.php">Staff List</a>
-							</div>
-	</div>
-	<a href="../PAGES/STAFFCONTACT.php">Contacts</a>
-	<a href="../PAGES/STAFFABOUTUS.php">About Us</a>
-	<a href="../ACCESS/stafflogout.php">Logout</a>
+		<a href="../PAGES/STAFFHOME">Home</a>
+		<div class="dropdown">
+			<button class="dropbtn">Projects
+				<i class="fa fa-caret-down"></i>
+			</button>
+	        	<div class="dropdown-content">
+	            		<a href="../PROJECT/ADDPROJECT">Create Project</a>
+				<a href="../PROJECT/PROJECTLIST">Project List</a>
+				<a href="../PROJECT/PROJECTUPDATE">Update Project</a>
+	            		<a href="../PROJECT/ADDGROUP">Create Group</a>
+	            		<a href="../PROJECT/GROUPLIST">Group List</a>
+				<a href="../PROJECT/GROUPUPDATE">Update Group</a>
+	                	<a href="../PROJECT/STUDENTLIST">Student List</a>
+	                	<a href="../PROJECT/STAFFLIST">Staff List</a>
+			</div>
+		</div>
+		<a href="../PAGES/STAFFCONTACT">Contacts</a>
+		<a href="../PAGES/STAFFABOUTUS">About Us</a>
+		<a href="../ACCESS/stafflogout">Logout</a>
 </div>
 
 <div id="contents">
@@ -58,7 +58,7 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 <p>This site aims to provide staff and students with a platform to view and request access to upcoming projects. Through this platform, staff and teachers may upload projects for students, including details of the skills sets and numbers required for the job. Students who register can then complete a quick survey to determine their levels or proficency in certain areas such as IT and communication. The Corral Project takes this information, and provides Teachers with a list of suitable candidates for the positions available.
 
 
-  <br><br>For more information, please click <a href="STAFFABOUTUS.PHP">here</a></p>
+  <br><br>For more information, please click <a href="../PAGES/STAFFABOUTUS">here</a></p>
 
   </div>
 </div>
