fa1878031c5157d24b9bf13195287f01a93966ba
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-27 16:26:16 +1000

    Updated for new page

--- PROJECT/STUDENTPROCESS.php
+++ PROJECT/STUDENTPROCESS.php
@@ -57,6 +57,7 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 					<ul>
 						<li><a href ="../PROJECT/STUDENTLIST">Student List</a></li>
 						<li><a href ="../PROJECT/STAFFLIST">Staff List</a></li>
+						<li><a href ="../PROJECT/MEMBERSEARCH">Search For</a></li>
 						</li>
 					</ul>
 				</li>
