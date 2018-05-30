7f89cb29c1af705215644bdf6441a51ab1f267ae
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-30 20:45:13 +1000

    Updated for status export

--- PROJECT/PROJECTLISTCSV.php
+++ PROJECT/PROJECTLISTCSV.php
@@ -16,12 +16,17 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 
 if(isset($_POST["export_excel"]))
 {
+	$Status = $_POST['View'];
 require('../DATABASE/CONNECTDB.PHP');
 	header('Content-Type: text/csv; charset=utf-8');
 	header('Content-Disposition: attachment; filename=data.csv');
 	$output = fopen("php://output", "w");
 	fputcsv($output, array('Project Number', 'Project Brief', 'Project Leader', 'Project Status'));
-	$query = "SELECT PROJECT_NUM, PROJECT_BRIEF, PROJECT_LEADER, PROJECT_STATUS FROM PROJECT";
+	if ($Status == "All"){
+		$query = "SELECT PROJECT_NUM, PROJECT_BRIEF, PROJECT_LEADER, PROJECT_STATUS FROM PROJECT";
+	} else {
+		$query = "SELECT PROJECT_NUM, PROJECT_BRIEF, PROJECT_LEADER, PROJECT_STATUS FROM PROJECT WHERE PROJECT_STATUS = '".$Status."'";
+	}
 	$result = mysqli_query($CON, $query);
 	while($row = mysqli_fetch_assoc($result))
 	{
