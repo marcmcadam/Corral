55bcdd885c7d69c85ba01cbbb1ef0c815985e086
Marc McAdam <mmcadam@deakin.edu.au>
2018-05-31 23:25:26 +1000

    Replacement survey pages for automation

---
+++ SURVEY/survey.php
@@ -0,0 +1,88 @@
+<html>
+<form action="surveyprocess.php" method="post">
+
+STUDENT_FIRSTNAME:<input type="text" name="STUDENT_FIRSTNAME">
+
+<p>Project skills</p>
+html/css<select name="hc">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Java Script<select name="js">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+PHP<select name="php">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+JAVA<select name="java">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+C<select name="c">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+C Plus Plus<select name="cpp">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Objective-C<select name="oc">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Databse<select name="db">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Unity<select name="u3">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+UI<select name="ui">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Security<select name="se">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+<p><input type="submit">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="reset" value="reset"></p>
+</form>
+
+
+</html>
