40945a8c0d69efb0f06702e8aae16c28c569a0b0
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-06-01 00:30:27 +1000

    Updated to looks

--- SURVEY/surveyprocess.php
+++ SURVEY/surveyprocess.php
@@ -1,4 +1,35 @@
-	<?php
+<?php
+session_start();
+if ( $_SESSION['STUDENT_ID'] != 1) {
+	$_SESSION['message'] = "You mus log in before viewing this page";
+	header("location: ../ACCESS/error");
+	}
+	else {
+	$id = $_SESSION['STUDENT_ID'];
+	$student_firstname = $_SESSION['STUDENT_FIRSTNAME'];
+	$student_lastname = $_SESSION['STUDENT_LASTNAME'];
+	}
+?>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="../STYLES/stylesheet.css">
+</head>
+
+<body>
+<div class="Header">
+	<h1>Corral</h1>
+</div>
+
+<div class="navbar">
+	<a href="../PAGES/STUDENTHOME">Home</a>
+	<a href="../SURVEY/STUDENTSURVEY">Survey</a>
+	<a href="../PAGES/STUDENTCONTACT">Contacts</a>
+	<a href="../PAGES/STUDENTABOUTUS">About Us</a>
+	<a href="../Access/LOGOUT">Logout</a>
+</div>	
+<?php
 
 	require('../DATABASE/CONNECTDB.PHP');
 
@@ -168,3 +199,11 @@
 
 
 	?>
+	<h2>Thank for completing the survey</h2>
+	<p>Please log out</p>
+	
+<div class="Footer">
+	<h4>© Copyright Deakin University & Group 29 2018</h4>
+</div>
+</body>
+</html>
