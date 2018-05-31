ea79fca97eb225d428f92e67963b2ed235736d2b
ZF-Deakin <kw.zacharyframe@gmail.com>
2018-05-31 23:21:54 +1000

    Updated for new table

--- PROJECT/PROJECTUPDATE.php
+++ PROJECT/PROJECTUPDATE.php
@@ -86,6 +86,85 @@ if ( $_SESSION['STAFF_ID'] != 1) {
 <p><textarea name="PROJECT_BRIEF" rows="5" cols="40" value="<?php echo $_GET['brief'];?>" id="ip3"></textarea></br></p>
 <p>Project Status &nbsp</br></br><input type="radio" name="PROJECT_STATUS" value="active"/>Active</br></br><input type="radio" name="PROJECT_STATUS" value="inactive"/>Inactive</br></br><input type="radio" name="PROJECT_STATUS" value="planning"/>Planning</br></br>
 <input type="radio" name="PROJECT_STATUS" value="cancelled"/>Cancelled</br></p>
+
+<p>Project skills</p>
+html/css<select name="hc">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Java Script<select name="js">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+PHP<select name="php">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+JAVA<select name="java">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+C<select name="c">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+C Plus Plus<select name="cpp">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Objective-C<select name="oc">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Databse<select name="db">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Unity<select name="u3">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+UI<select name="ui">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
+Security<select name="se">
+<option value="Expert">Expert</option>
+<option value="High">High</option>
+<option value="Intermediate">Intermediate</option>
+<option value="Novice">Novice</option>
+<option value="not required" selected>Not required</option></select></br></br>
+
 <p><input type="submit" value="update">&nbsp&nbsp<input type="reset" value="reset"></p>
 </form>
 </div>
