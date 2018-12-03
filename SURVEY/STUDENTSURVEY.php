3111679c0987b4acabf88eb3db07a7eee22bdbf1
Andrew Pearce <apearce@deakin.edu.au>
2018-12-04 00:33:26 +1100

    Alterations to the solver and survey pages

--- SURVEY/STUDENTSURVEY.php
+++ SURVEY/STUDENTSURVEY.php
@@ -1,92 +1,42 @@
 <?php
  	$PageTitle = "Student Survey";
 	require "../PAGES/HEADER_STUDENT.PHP";
+    
+    function skillOptions($title, $key)
+    {
+        echo "<tr>
+            <td>$title</td>
+            <td><select name='$key'>
+                <option value='4'>Expert</option>
+                <option value='3'>High</option>
+                <option value='2'>Intermediate</option>
+                <option value='1'>Novice</option>
+                <option value='0' selected>None</option>
+            </select></td>
+            </tr>";
+    }
 ?>
 
 <h2>Student Survey</h2>
 <form action="../SURVEY/surveyprocess" method="post">
 
-<p>STUDENT_FIRSTNAME: <input type="text" name="STUDENT_FIRSTNAME" id="ip2"></p>
-
 <p>Project skills</p>
-<p>html/css: <select name="hc">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>Java Script: <select name="js">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>PHP: <select name="php">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>JAVA: <select name="java">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>C: <select name="c">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>C Plus Plus: <select name="cpp">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>Objective-C: <select name="oc">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>Databse: <select name="db">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>Unity: <select name="u3">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>UI: <select name="ui">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p>Security: <select name="se">
-<option value="Expert">Expert</option>
-<option value="High">High</option>
-<option value="Intermediate">Intermediate</option>
-<option value="Novice">Novice</option>
-<option value="not required" selected>Not required</option></select></p>
-
-<p><input type="submit">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="reset" value="reset"></p>
+<table>
+<?php
+    skillOptions("HTML/CSS", "hc");
+    skillOptions("JavaScript", "js");
+    skillOptions("PHP", "php");
+    skillOptions("Java", "java");
+    skillOptions("C", "c");
+    skillOptions("C++", "cpp");
+    skillOptions("Objective-C", "oc");
+    skillOptions("Database", "db");
+    skillOptions("Unity", "u3");
+    skillOptions("UI", "ui");
+    skillOptions("Security", "se");
+?>
+</table>
+<input type="submit" value="Send Responses">
 </form>
 
 <?php require "../PAGES/FOOTER_STUDENT.PHP"; ?>
