0ce00a90b901d08f9822a207a13052595ac40e22
Brian King <kingbr@deakin.edu.au>
2018-12-06 00:01:16 +1100

    Added database lookup, send email functionality.

---
+++ ACCESS/resetpassword.php
@@ -0,0 +1,12 @@
+<?php
+require "../DATABASE/CONNECTDB.PHP";
+$PageTitle = "Login Page";
+require "../PAGES/HEADER_PUBLIC.PHP";
+
+// NYI
+
+?>
+
+<h2>Not Yet Implemented</h2>
+
+<?php require "../PAGES/FOOTER_PUBLIC.PHP"; ?>
