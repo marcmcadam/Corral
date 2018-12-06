46bd1a0aac5e34dc9f7da4f8420f14f0121bb29b
Andrew Pearce <apearce@deakin.edu.au>
2018-12-07 09:55:59 +1100

    Database implementation of min, max, importance and background stopping.

--- SOLVER/Hungarian.php
+++ SOLVER/Hungarian.php
@@ -267,7 +267,6 @@ class Hungarian
         $non_covered_zero_matrix = $this->getNonCoveredZeroMatrix($zero_matrix);
         while ($non_covered_zero_matrix) {
             $timeOut -= 1;
-            set_time_limit(30);
             if ($timeOut == 0)
                 return null;
             /*
@@ -362,7 +361,6 @@ class Hungarian
         }
 
         $timeOut -= 1;
-        set_time_limit(30);
         if ($timeOut == 0)
             return null;
         
