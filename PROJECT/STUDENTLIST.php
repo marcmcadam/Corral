ddf0f420e27fbdc0cab814c03d00d3c4cacb2d4f
Andrew Pearce <apearce@deakin.edu.au>
2018-12-08 09:38:53 +1100

    Improved sorting, tweaks and bug fixes

--- PROJECT/STUDENTLIST.php
+++ PROJECT/STUDENTLIST.php
@@ -3,13 +3,13 @@
 	require "../PAGES/HEADER_STAFF.PHP";
 ?>
 <h2>Student Information</h2>
-<form action="STUDENT_CSV" method="post">
+<form action="STUDENTCSV" method="post">
 	<input type="submit" value="Export Student List To CSV">
 </form>
 <form action="STUDENTPDF" method="post">
 	<input type="submit" value="Export Student List To PDF">
 </form>
-<form action="SURVEY_CSV" method="post">
+<form action="SURVEYCSV" method="post">
 	<input type="submit" value="Export Student Survey To CSV">
 </form>
 <br>
@@ -19,14 +19,16 @@
     $sql = "SELECT * FROM student ORDER BY stu_ID ASC";
     $res = mysqli_query($CON, $sql);
 
-    echo "<p><table width='1250px' height='150px' border='1px' cellpadding='10px' align='center'></p>";
-    echo "<tr>  <th>ID</th>
-                <th>FirstName</th>
-                <th>LastName</th>
-                <th>Campus</th>
-                <th>Email</th>
-                <th>Survey</th>
-                <th>Update Information</th>
+    echo "<table width='1250px' border='1px' cellpadding='8px' align='center'>
+        <tr>
+            <th>ID</th>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>Campus</th>
+            <th>Email</th>
+            <th>Survey</th>
+            <th>Project</th>
+            <th>Update</th>
         </tr>";
 
     while ($row = mysqli_fetch_assoc($res))
@@ -52,11 +54,47 @@
                 $surveyDone = "$count!";
         }
         
+        $groupsSQL = "SELECT * FROM groups WHERE stu_Id = $stu_id";
+        $groups = mysqli_query($CON, $groupsSQL);
+        
+        if (!$groups)
+            $projectText = "?";
+        else
+        {
+            $projectTitles = [];
+            while ($groupRow = mysqli_fetch_assoc($groups))
+            {
+                $pro_num = $groupRow["pro_num"];
+                
+                $found = false;
+                if (!is_null($pro_num))
+                {
+                    $projectSQL = "SELECT * FROM project WHERE pro_num = $pro_num";
+                    $projects = mysqli_query($CON, $projectSQL);
+                    
+                    if ($projects)
+                    {
+                        while ($projectRow = mysqli_fetch_assoc($projects))
+                        {
+                            $found = true;
+                            array_push($projectTitles, $projectRow["pro_title"]);
+                        }
+                    }
+                }
+                if (!$found)
+                    array_push($projectTitles, "?");
+            }
+            if (sizeof($projectTitles) == 0)
+                $projectText = "-";
+            else
+                $projectText = join(", ", $projectTitles);
+        }
+        
         echo "<tr>
-            <td align='center' width='70px'>$stu_id</td>
-            <td align='center' width='190px'>{$row['stu_FirstName']}</td>
-            <td align='center' width='190px'>{$row['stu_LastName']}</td>
-            <td align='center' width='180px'>";
+            <td align='center'>$stu_id</td>
+            <td align='center'>{$row['stu_FirstName']}</td>
+            <td align='center'>{$row['stu_LastName']}</td>
+            <td align='center'>";
         switch ($row["stu_Campus"]) {
           case 1:
             echo "Burwood";
@@ -69,8 +107,9 @@
             break;
         }
         echo "</td>
-                <td align='center' width='500px'>{$row['stu_Email']}</td>
-                <td align='center' width='70px'>$surveyDone</td>
+                <td align='center'>{$row['stu_Email']}</td>
+                <td align='center'>$surveyDone</td>
+                <td align='center'>$projectText</td>
                 <td align='center'><a href='STUDENTUPDATE.php?number={$row['stu_ID']}&firstname={$row['stu_FirstName']}&lastname={$row['stu_LastName']}&location={$row['stu_Campus']}&email={$row['stu_Email']}'>Update</a></td>
         </tr>";
     }
