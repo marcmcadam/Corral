43dee6c45f5d09b87c1e83af6436786379d8a239
Brian King <kingbr@deakin.edu.au>
2018-12-09 12:59:02 +1100

    Fixed projectlist.php and projectlistcsv

--- PROJECT/PROJECTLISTCSV.php
+++ PROJECT/PROJECTLISTCSV.php
@@ -1,31 +1,29 @@
 <?php
 session_start();
 
-if ( $_SESSION['STAFF_ID'] != 1) {
-	$_SESSION['message'] = "You mus log in before viewing this page";
-	header("location: ../ACCESS/error");
-	}
-	else {
+if ( !isset($_SESSION['STAFF_ID'])) {
+	$_SESSION['message'] = "You must log in before viewing this page";
+	header("location: ../ACCESS/STAFFLOGIN.PHP");
+} else {
 	$id = $_SESSION['STAFF_ID'];
 	$staff_firstname = $_SESSION['STAFF_FIRSTNAME'];
 	$staff_lastname = $_SESSION['STAFF_LASTNAME'];
-	}
+}
 
 
+$view = ["All", "Active","Inactive","Planning","Cancelled"];
 
-
-if(isset($_POST["export_excel"]))
-{
-	$Status = $_POST['View'];
-require('../DATABASE/CONNECTDB.PHP');
+if(isset($_POST['View']) && in_array($_POST['View'], $view)) {
+	$status = $_POST['View'];
+	require_once "../DATABASE/CONNECTDB.PHP";
 	header('Content-Type: text/csv; charset=utf-8');
-	header('Content-Disposition: attachment; filename=data.csv');
+	header('Content-Disposition: attachment; filename=projectlist.csv');
 	$output = fopen("php://output", "w");
-	fputcsv($output, array('Project Number', 'Project Title', 'Project Brief', 'Project Leader', 'Project Email', 'Project Status'));
-	if ($Status == "All"){
-		$query = "SELECT pro_num, pro_title, pro_brief, pro_leader, pro_email, pro_status FROM project";
+	fputcsv($output, array('Project Title', 'Project Brief', 'Project Leader', 'Project Email', 'Project Status'));
+	if ($status == "All"){
+		$query = "SELECT pro_title, pro_brief, pro_leader, pro_email, pro_status FROM project";
 	} else {
-		$query = "SELECT pro_num, pro_title, pro_brief, pro_leader, pro_email, pro_status FROM project WHERE pro_status = '".$Status."'";
+		$query = "SELECT pro_title, pro_brief, pro_leader, pro_email, pro_status FROM project WHERE pro_status = '".$status."'";
 	}
 	$result = mysqli_query($CON, $query);
 	while($row = mysqli_fetch_assoc($result))
