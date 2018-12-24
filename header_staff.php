08bf2ffe942508a021e1d27a3b8b328888bb84ad
Brian King <kingbr@deakin.edu.au>
2018-12-24 15:10:02 +1100

    Added search bar

--- header_staff.php
+++ header_staff.php
@@ -19,6 +19,7 @@ else
 <meta charset="utf-8">
 <title><?php echo $PageTitle; ?></title>
 <link rel="stylesheet" type="text/css" href="stylesstaff.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!-- Icon Library CSS -->
 <link rel="icon" type="image/ico" href="favicon.ico">
 <?php echo isset($script) ? $script : "" ; // Echo header script if one exists (JavaScript Validation etc)?>
 </head>
@@ -46,9 +47,13 @@ else
 				<li class="menu"><a href ="datamgmt.php"><p>Manage Corral Data</p></a></li>
 				<li class="menu"><a href ="begin.php"><p>Begin Sort</p></a></li>
 				<li class="menu"><a href ="sortedgroups.php"><p>Sort Results</p></a></li>
+				<li class="menu"><a href ="logout.php"><p>Logout</p></a></li>
 			</ul>
 		</li><!--
-		--><li class="menu"><a href ="logout.php"><p>Logout</p></a></li>
+		--><li><form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form></li>
 	</ul>
 </div>
 <div class="main">
