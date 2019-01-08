081a7f44cdb5310b23fe2c4271d5bf51c953d85f
Andrew Pearce <apearce@deakin.edu.au>
2019-01-08 23:54:14 +1100

    Section for unassigned students on project groups page.

--- grouplist.php
+++ grouplist.php
@@ -1,6 +1,6 @@
 <?php
- 	  $PageTitle = "Project Groups";
-	  require "header_staff.php";
+    $PageTitle = "Project Groups";
+    require "header_staff.php";
     require_once "connectdb.php";
     require_once "getdata.php";
     require_once "getfunctions.php";
@@ -10,24 +10,61 @@
 
     echo "<h2>$PageTitle</h2>
         <table align='center' cellpadding='8px' style='width: 100%;'>";
+    // display unassigned students
+    {
+        $unassigned = [];
+        foreach ($students as $y => $student)
+        {
+            if (is_null($student->projectIndex))
+                array_push($unassigned, $student);
+        }
+        
+        $unassignedCount = sizeof($unassigned);
+        echo "  <tr>
+                    <td valign='top' style='text-align: right;'>
+                    $unassignedCount Unassigned Students
+                    </td><td valign='top'><table align='left' class='listTable'>
+                        <tr>";
+        if ($unassignedCount > 0)
+        {
+            echo "      <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Campus</th>";
+        }
+        echo "      </tr>";
+        foreach ($unassigned as $student)
+        {
+            $campus = getCampus($student->campus);
+            echo "      <tr>
+                            <td style='text-align: right; font-family: monospace;'>$student->id</td>
+                            <td style='text-align: left;'>$student->text</td>
+                            <td style='text-align: left;'>$student->email</td>
+                            <td style='text-align: left;'>$campus</td>
+                        </tr>";
+        }
+        echo "      </table></td>
+                </tr>";
+    }
+    // display projects
     foreach ($projects as $p => $project)
     {
         echo "  <tr>
                     <td valign='top'><table align='right' class='listTable'>
                         <tr>
-                            <th>Title:</th>
+                            <th>Title</th>
                             <td style='text-align: left;'>$project->title</td>
                         </tr><tr>
-                            <th>Brief:</th>
+                            <th>Brief</th>
                             <td style='text-align: left;'>$project->brief</td>
                         </tr><tr>
-                            <th>Leader:</th>
+                            <th>Leader</th>
                             <td style='text-align: left;'>$project->leader</td>
                         </tr><tr>
-                            <th>Email:</th>
+                            <th>Email</th>
                             <td style='text-align: left;'>$project->email</td>
                         </tr><tr>
-                            <th>Members:</th>
+                            <th>Members</th>
                             <td style='text-align: left;'>$project->allocation</td>
                         </tr>
                     </table></td><td valign='top'><table align='left' class='listTable'>
@@ -49,9 +86,9 @@
                         </tr>";
         }
         echo "      </table></td>
-                <tr>";
+                </tr>";
     }
-    echo "  </tr></table>";
+    echo "  </table>";
 
     require "footer.php";
 ?>
