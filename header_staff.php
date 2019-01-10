a14dc4f2f8c78de0f9e22ef7b009d53a98d59702
Andrew Pearce <apearce@deakin.edu.au>
2019-01-10 11:19:05 +1100

    Unit select on home page.

--- header_staff.php
+++ header_staff.php
@@ -3,20 +3,6 @@
     require "staffauth.php";
     require_once "getfunctions.php";
     require_once "connectdb.php";
-
-    $parsedURL = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-    $parsedParts = explode("/", $parsedURL);
-    $pagePart = $parsedParts[sizeof($parsedParts) - 1];
-    $pageNoGet = explode("?", $pagePart)[0];
-    if ($pageNoGet == "header_staff" && isset($_GET["unit"]))
-    {
-        $_SESSION["unit"] = (string)$_GET["unit"]; // TODO: needs validation?
-        if (!isset($_GET["return"]))
-            die("No return address.");
-        $return = $_GET["return"]; // TODO: needs validation?
-        header("location: $return");
-        die;
-    }
 ?>
 <!DOCTYPE html>
 <html>
@@ -53,25 +39,7 @@
             <li><a href="stafflist"><p>Staff</p></a></li>
             <li><a href="datamgmt"><p>Manage Corral Data</p></a></li>
         </ul>
-    </div><div class="menuGap" style="width: 15%;">
-        <?php
-            $units = getUnits($CON);
-            $page = $_SERVER["REQUEST_URI"];
-            echo "<form action='header_staff'>
-                    <select class='updateList' name='unit' onchange='this.form.submit()'>";
-                    $i=0;
-                    while (isset($units[$i])) {
-                        echo "<option value='".$units[$i]."'";
-                        if ($units[$i] === $unitID)
-                            echo " selected";
-                        echo ">".$units[$i]."</option>";
-                        $i++;
-                    }
-                    echo "</select>
-                    <input hidden type='text' class='updateButton' name='return' value='$page'>
-                </form>";
-        ?>
-    </div><div class="menuGap" style="width: 15%;">
+    </div><div class="menuGap" style="width: 20%;">
         <form action="search" method="get">
             <input type="text" placeholder="Search.." name="search" class='updateBox' style='max-width: 128px;'>
         </form>
