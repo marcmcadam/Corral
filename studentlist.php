edda222e2ef7e2e40ce8233a963b1fe50aacd0db
Andrew Pearce <apearce@deakin.edu.au>
2019-01-15 10:20:02 +1100

    Students page is made unit specific.

--- studentlist.php
+++ studentlist.php
@@ -3,102 +3,47 @@
 	require "header_staff.php";
     require_once "getfunctions.php";
     require_once "connectdb.php";
+    require_once "unitdata.php";
 
-    $sql = "SELECT * FROM student ORDER BY stu_ID ASC";
-    $res = mysqli_query($CON, $sql);
+    $unitData = unitData($unitID);
+    $skillNames = $unitData->skillNames;
+    $sort = $unitData->sort;
+    $students = $unitData->students;
+    $projects = $unitData->projects;
+    $unassigned = $unitData->unassigned;
 
     echo "<h2>Student Information</h2>
     <form name ='studentListForm' action='studentuser'  method='get'>
     <table class='listTable' align='center'>
         <tr>
             <th>ID</th>
-            <th>FirstName</th>
-            <th>LastName</th>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Campus</th>
-            <th>Email</th>";
-            /*
+            <th>Email</th>
             <th>Survey</th>
             <th>Project</th>
-            */
-    echo "  <th>Update</th>
+            <th>Update</th>
         </tr>";
 
-while ($row = mysqli_fetch_assoc($res))
+foreach ($students as $y => $student)
 {
-    $stu_id = $row["stu_ID"];
-    /*
-    $surveySQL = "SELECT a.stu_Id as 'stu_Id' FROM surveyanswer a WHERE a.stu_Id = $stu_id";
-    $surveys = mysqli_query($CON, $surveySQL);
+    $surveyDone = ($student->skills == null) ? "-" : "Y";
+    $projectTitle = ($student->projectIndex != null) ? $projects[$student->projectIndex]->title : "-";
+    $campus = getCampus($student->campus);
 
-    if (!$surveys)
-        $surveyDone = "?";
-    else
-    {
-        $count = 0;
-        while ($surveyRow = mysqli_fetch_assoc($surveys))
-            $count += 1;
-
-        if ($count == 0)
-            $surveyDone = "-";
-        else if ($count == 1)
-            $surveyDone = "Y";
-        else
-            $surveyDone = "$count!";
-    }
-
-    $groupsSQL = "SELECT * FROM surveyanswer WHERE stu_Id=$stu_id AND unit_ID='$unitID'";
-    $groups = mysqli_query($CON, $groupsSQL);
-    if (!$groups)
-    {
-        $error = mysqli_error($CON);
-        $projectText = "$error";
-    }
-    else
-    {
-        $projectTitles = [];
-        while ($groupRow = mysqli_fetch_assoc($groups))
-        {
-            $pro_ID = $groupRow["pro_ID"];
-
-            $found = false;
-            if (!is_null($pro_ID))
-            {
-                $projectSQL = "SELECT * FROM project WHERE pro_ID = $pro_ID";
-                $projects = mysqli_query($CON, $projectSQL);
-
-                if ($projects)
-                {
-                    while ($projectRow = mysqli_fetch_assoc($projects))
-                    {
-                        $found = true;
-                        array_push($projectTitles, $projectRow["pro_title"]);
-                    }
-                }
-            }
-            if (!$found)
-                array_push($projectTitles, "?");
-        }
-        if (sizeof($projectTitles) == 0)
-            $projectText = "-";
-        else
-            $projectText = join(", ", $projectTitles);
-    }
-    */
     echo "<tr>
-        <td align='center'>$stu_id</td>
-        <td align='center'>{$row['stu_FirstName']}</td>
-        <td align='center'>{$row['stu_LastName']}</td>
-        <td align='center'>".getcampus($row["stu_Campus"])."</td>
-        <td align='center'>{$row['stu_Email']}</td>";
-        /*
+        <td align='center'>$student->id</td>
+        <td align='center'>$student->firstName</td>
+        <td align='center'>$student->lastName</td>
+        <td align='center'>$campus</td>
+        <td align='center'>$student->email</td>
         <td align='center'>$surveyDone</td>
-        <td align='center'>$projectText</td>
-        */
-    echo "<td align='center'><button value ='".$stu_id."' name='studentid' class='updateButton'>Update</button></td>
+        <td align='center'>$projectTitle</td>
+        <td align='center'><button value ='$student->id' name='studentid' class='updateButton'>Update</button></td>
     </tr>";
 }
 
 echo "</table>";
-mysqli_free_result($res);
-mysqli_close($CON);
+
 require "footer.php"; ?>
