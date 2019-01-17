4eebdc0b6b2259784164116b1e741d11cfb329e4
Andrew Pearce <apearce@deakin.edu.au>
2019-01-17 16:08:50 +1100

    Tooltips for sorting setup page.

--- begin.php
+++ begin.php
@@ -11,56 +11,15 @@
     $projects = $unitData->projects;
     $unassigned = $unitData->unassigned;
 ?>
-<script>
-    var helps = [];
-    helps["start"] = "";
-    helps["iterations"] = "Sorting will automatically stop if this number of iterations are reached.<br><br><br>";
-    helps["matrix"] = "The sorting will start at low quality and increase until it reaches this number.<br><br>This is how many students are compared at once. Increasing this:<br>- Allows more complex swaps, and will have a better result.<br>- Risks the server calculations timing-out or using too much memory.<br>Use the maximum available for server limits.<br><br><br>";
-
-    function showHelp(helpName)
-    {
-        if (document.getElementById(helpName).innerHTML == "")
-            document.getElementById(helpName).innerHTML = helps[helpName];
-        else
-            document.getElementById(helpName).innerHTML = "";
-    }
-</script>
-<style>
-    td {
-        max-width: 75%;
-    }
-</style>
 <form action="begin2.php" method="post" target="_blank">
     <h2>Sorting Setup</h2>
-    <table align='center'>
-        <tr>
-            <td>
-                <input type="submit" value="Start Sorting" class="inputButton"><br>
-                <br>
-            </td><!--<td style="min-width: 64px; text-align: left;">
-                <input type="button" id="start?" value="?" onclick="showHelp('start');" class="updateButton"><br>
-                <br>
-            </td>-->
-        </tr><tr>
-            <td><div id="start" style='text-align: left; font-size: 0.75em;'></div></td>
-        </tr><tr>
-            <td colspan="2">
-                Maximum Running Time<br><input class="inputBox" type="text" name="iterations" value="<?php echo $sort->iterations; ?>">
-                <input type="button" id="iterations?" value="?" onclick="showHelp('iterations');" class="updateButton"><br>
-                <br>
-            </td>
-        </tr><tr>
-            <td><div id="iterations" style='text-align: left; font-size: 0.75em;'></div></td>
-        </tr><tr> 
-            <td colspan="2">
-                Maximum Quality<br><input class="inputBox" type="text" name="matrix" value="<?php echo $sort->matrix; ?>">
-                <input type="button" id="matrix?" value="?" onclick="showHelp('matrix');" class="updateButton"><br>
-                <br>
-            </td>
-        </tr><tr>
-            <td><div id="matrix" style='text-align: left; font-size: 0.75em;'></div></td>
-        </tr>
-    </table>
+    <input type="submit" value="Start Sorting" class="inputButton"><br>
+    <br><br>
+    Maximum Running Time<br><input class="inputBox" type="text" name="iterations" value="<?php echo $sort->iterations; ?>">
+    <span class='tooltip'> ?<span class='tooltiptext'>Sorting will automatically stop if this number of iterations are reached.</span></span>
+    <br><br>
+    Quality<br><input class="inputBox" type="text" name="matrix" value="<?php echo $sort->matrix; ?>">
+    <span class='tooltip'> ?<span class='tooltiptext'>This is how many students are compared at once. Increasing this:<br>- Allows more complex swaps, and will have a better result.<br>- Risks the server calculations timing-out or using too much memory.<br><br>Use the maximum available for server limits.</span></span>
 </form>
 <?php
     require "footer.php";
